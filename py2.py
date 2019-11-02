9fc59a415e2791d40c75b7bb06cfaa619473ef2b
thisdjango <tsarkova.92@mail.ru>
2019-11-02 19:03:45 +0300

    second task

---
+++ py2.py
@@ -0,0 +1,10 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+second_integer = int(input())
+second_double = float(input())
+second_string = input()
+
+print(i + second_integer)
+print(d + second_double)
+print(s + second_string)
